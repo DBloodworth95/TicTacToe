d34c16f77bbfa99ad5e5d44ed6179e1a7be730b6
dan bloodworth <daniel.bloodworth18@my.northampton.ac.uk>
2020-05-26 14:56:37 +0100

    Finish main menu - Implement Listener for play button.

--- src/main/java/com/tictactoe/game/MainMenu.java
+++ src/main/java/com/tictactoe/game/MainMenu.java
@@ -7,23 +7,31 @@ import java.awt.event.ActionListener;
 
 public class MainMenu extends JFrame implements ActionListener {
 
-    private final JPanel mainPanel = new JPanel();
-
     private final JLabel title = new JLabel("Welcome to Dan's TicTacToe Game!");
+    private final JLabel usernameL = new JLabel("Username:");
+
+    private final JTextField usernameTF = new JTextField();
+
+    private final JButton playB = new JButton("Play!");
 
     private static final int WIDTH = 3;
 
     private static final int HEIGHT = 3;
 
     public void construct() {
-        setLayout(new BorderLayout());
-        add(mainPanel, BorderLayout.CENTER);
-        mainPanel.setLayout(new GridLayout(WIDTH, HEIGHT));
-        mainPanel.add(title);
-        setTitle("Dan's Tic-Tac-Toe!");
+        title.setBounds(170,50,250,10);
+        usernameL.setBounds(170, 252, 100, 10);
+        usernameTF.setBounds(300, 250, 150, 20);
+        playB.setBounds(245, 400, 100,20);
+        add(title);
+        add(usernameL);
+        add(usernameTF);
+        add(playB);
+        setSize(600,600);
+        setLayout(null);
         setVisible(true);
+        setTitle("Dan's Tic-Tac-Toe!");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(800, 800, 600, 600);
     }
 
 
